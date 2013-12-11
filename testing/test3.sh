0570233c9b72ed108cd8550b46707e5807b7677c
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-12-11 11:10:50 -0800

    try reset before commit

---
+++ testing/test3.sh
@@ -0,0 +1,23 @@
+# assumes environment is set up as in test1.sh and 
+# that test1.sh has already been run
+
+echo "make a non-conflicting change in git"
+olddir=`pwd`
+
+tmpdir=`mktemp -d -t foobar`
+echo "tmpdir is $tmpdir"
+
+cd $tmpdir
+
+git clone git@github.com:$GITHUB_USERNAME/$REPO_NAME.git
+cd $REPO_NAME
+
+
+echo "add a non-conflicting line in git" >> README.md
+
+git commit -a -m "add a non-conflicting line in git"
+git push
+
+cd $olddir
+#rm -rf $tmpdir
+
