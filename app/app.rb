c658b9298916e61388db17ddd857f41b76bb232b
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-12-11 18:01:12 -0800

    remove squashes

--- app/app.rb
+++ app/app.rb
@@ -155,7 +155,7 @@ EOF
                 end
                 puts2("git commit message is:\n#{commit_msg}")
                 ###result = run %Q(git merge -m "#{eq(commit_msg)}" --commit --no-ff master)
-                run("git merge --squash master")
+                run("git merge master")
                 #result = run("git merge master")
                 if (result.first == 0)
                     puts2 "no problems with git merge"
