1e1ca0967867d4b33baae5290655029512538f29
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-12-23 18:29:46 -0800

    use a more universal url in cache_credentials

--- app/app.rb
+++ app/app.rb
@@ -230,7 +230,8 @@ helpers do
     # between the call to cache_credentials and the svn dcommit (or whatever) afterwards
     # so call this inside a block passed to exclusive_lock().
     def cache_credentials(username, password)
-        url = `git config --get svn-remote.hedgehog.url`.chomp
+        #url = `git config --get svn-remote.hedgehog.url`.chomp
+        url = SVN_URL
         puts2("in cache_credentials")
         # fixme do this on production only?
         puts2("removing auth directory...")
