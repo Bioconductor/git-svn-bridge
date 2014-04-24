f65edb386540192e03aadcacf98787a34079814a
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-04-24 14:40:56 -0700

    Add a script to make sure apache stays up

---
+++ app/pinger.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# This should be run as root (by cron maybe).
+# It makes sure the server is up.
+
+res = `curl -I https://gitsvn.bioconductor.org`
+
+lines = res.split "\n"
+
+responsecode = lines.find{|i| i =~ /^HTTP\/1\.1 /}.split(" ")[1]
+
+unless responsecode == "200"
+    `service apache2 restart`
+end
